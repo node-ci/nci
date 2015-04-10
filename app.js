9a499248dfb612eb505c607b71d61c8fad1449f7
Vladimir Polyakov <nrd11k@gmail.com>
2015-04-10 23:17:03 +0300

    react through requirejs

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 
 var http = require('http');
 var nodeStatic = require('node-static');
+var jade = require('jade');
 
 var staticServer = new nodeStatic.Server('./static');
 var app = http.createServer(function(req, res, next) {
 	// serve index for all app pages
-	if (req.url.indexOf('/builds') === 0) {
-		staticServer.serveFile('/index.html', 200, {}, req, res);
-	} else {
-		if (req.url.indexOf('/data.io.js') === -1) {
+	if (req.url.indexOf('/data.io.js') === -1) {
+		if (req.url.indexOf('/js') === -1) {
+			// Compile a function
+			console.log(1234);
+			var index = jade.compileFile(__dirname + '/views/index.jade');
+			res.write(index());
+			res.end();
+		} else {
 			staticServer.serve(req, res);
 		}
 	}
---
+++ static/js/app/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+define(['react'], function(React) {
+	console.log(React);
+});
