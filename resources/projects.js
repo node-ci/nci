bea76d88738123e51e1009b06a58b14559ab4f7c
oleg <oleg.korobenko@gmail.com>
2015-04-13 01:19:42 +0300

    add sample projects and their loading

--- resources/projects.js
+++ resources/projects.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var _ = require('underscore'),
+	project = require('../lib/project');
+
+var projects;
+project.loadAll('projects', function(err, loadedProjects) {
+	if (err) throw err;
+	projects = loadedProjects;
+	console.log('Loaded projects: ', _(projects).map(function(project) {
+		return project.config.name;
+	}));
+});
+
 module.exports = function(data) {
 	var projects = [{
 		name: 'foo'
