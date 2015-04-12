637ad82f5c17650215c3b78f9ddedce43c54578e
oleg <oleg.korobenko@gmail.com>
2015-04-13 01:56:47 +0300

    show names of loaded projects on the client

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,15 +1,20 @@
 'use strict';
 
 define([
-	'react', 'socketio', 'dataio'
+	'underscore', 'react', 'socketio', 'dataio'
 ], function(
-	React, socketio, dataio
+	_, React, socketio, dataio
 ) {
+	var contentEl = window.document.getElementById('content');
+
 	// console.log(React, socketio, dataio);
 	var connect = dataio(socketio.connect());
 
 	var projects = connect.resource('projects');
 	projects.sync('read', function(err, projects) {
-		console.log('>>> err, projects = ', err, projects)
+		contentEl.innerHTML = (
+			(err && err.message) ||
+			('Loaded projects: ' + _(projects).pluck('name').join(', '))
+		);
 	});
 });
--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,7 @@
 require.config({
 	baseUrl: '/js/',
 	paths: {
+		underscore: 'libs/underscore/underscore',
 		react: 'libs/react/react-with-addons',
 		dataio: '/data.io',
 		socketio: '/socket.io/socket.io.js'
