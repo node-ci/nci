d53744bab7d7592358dacd3f1056c0d46d0c37fd
Vladimir Polyakov <nrd11k@gmail.com>
2015-05-10 00:47:01 +0300

    add rebuild project button

--- static/js/app/components/builds/item.js
+++ static/js/app/components/builds/item.js
@@ -1,15 +1,19 @@
 'use strict';
 
 define([
-	'react', 'app/actions/build', 'templates/app/components/builds/item',
+	'react', 'app/actions/project',
+	'app/actions/build', 'templates/app/components/builds/item',
 	'app/components/common/index'
-], function(React, BuildActions, template, CommonComponents) {
+], function(React, ProjectActions, BuildActions, template, CommonComponents) {
 	template = template.locals({
 		DateTime: CommonComponents.DateTime
 	});
 	console.log(CommonComponents.DateTime);
 
 	var Component = React.createClass({
+		onRebuildProject: function(projectName) {
+			ProjectActions.run(projectName)
+		},
 		onBuildSelect: function(buildId) {
 			console.log('on build select');
 			BuildActions.readConsoleOutput(buildId);
