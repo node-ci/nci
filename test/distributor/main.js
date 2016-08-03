97835b90029310962863ac99dd1f8977c6ea92ca
oleg <oleg.korobenko@gmail.com>
2016-08-03 21:50:50 +0300

    add project envs (without build group id) and tests for envs

--- test/distributor/main.js
+++ test/distributor/main.js
@@ -265,4 +265,124 @@ describe('Distributor main', function() {
 		});
 	});
 
+	describe('with success project and build with env', function() {
+		var updateBuildSpy;
+
+		it('instance should be created without errors', function() {
+			distributor = helpers.createDistributor({
+				projects: projects,
+				nodes: [{type: 'local', maxExecutorsCount: 1, envs: ['env1']}]
+			});
+			updateBuildSpy = sinon.spy(distributor, '_updateBuild');
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: 'project1', env: 'env1'}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		it('build should be queued with env', function() {
+			var changes = updateBuildSpy.getCall(0).args[1];
+			expect(changes).have.keys('status', 'env');
+			expect(changes.status).equal('queued');
+			expect(changes.env).eql({name: 'env1'});
+		});
+
+		it('build should be in-progress', function() {
+			var changes = updateBuildSpy.getCall(1).args[1];
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('in-progress');
+		});
+
+		it('build should be done', function() {
+			var changes = updateBuildSpy.getCall(2).args[1];
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('done');
+		});
+
+		it('update build called 3 times in total', function() {
+			expect(updateBuildSpy.callCount).equal(3);
+		});
+	});
+
+	describe('with success project and two project envs', function() {
+		var updateBuildSpy;
+
+		it('instance should be created without errors', function() {
+			var envs = ['env1', 'env2'];
+			distributor = helpers.createDistributor({
+				projects: [{name: 'project1', envs: envs}],
+				nodes: [{type: 'local', maxExecutorsCount: 1, envs: envs}]
+			});
+			updateBuildSpy = sinon.spy(distributor, '_updateBuild');
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: 'project1'}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		it('build for first env should be queued', function() {
+			var args = updateBuildSpy.getCall(0).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env1'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('queued');
+		});
+
+		it('build for second env should be queued', function() {
+			var args = updateBuildSpy.getCall(1).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env2'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('queued');
+		});
+
+		it('build for first env should be in-progress', function() {
+			var args = updateBuildSpy.getCall(2).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env1'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('in-progress');
+		});
+
+		it('build for first should be done', function() {
+			var args = updateBuildSpy.getCall(3).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env1'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('done');
+		});
+
+		it('build for second env should be in-progress', function() {
+			var args = updateBuildSpy.getCall(4).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env2'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('in-progress');
+		});
+
+		it('build for second should be done', function() {
+			var args = updateBuildSpy.getCall(5).args,
+				build = args[0],
+				changes = args[1];
+			expect(build.env).eql({name: 'env2'});
+			expect(changes).have.keys('status');
+			expect(changes.status).equal('done');
+		});
+
+		it('update build called 6 times in total', function() {
+			expect(updateBuildSpy.callCount).equal(6);
+		});
+	});
+
 });
