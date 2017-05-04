16e5ee2690bbf1e568dc9e6adb07e0365b13f49d
oleg <oleg.korobenko@gmail.com>
2017-05-04 16:30:01 +0300

    done with tests for projects load config

--- test/collections/project/loadConfig.js
+++ test/collections/project/loadConfig.js
@@ -3,7 +3,8 @@
 var _ = require('underscore'),
 	expect = require('expect.js'),
 	sinon = require('sinon'),
-	path = require('path');
+	path = require('path'),
+	utils = require('../../../lib/utils');
 
 describe('Projcts collection `loadConfig` method', function() {
 
@@ -50,14 +51,40 @@ describe('Projcts collection `loadConfig` method', function() {
 		}
 	};
 
-	describe('with config in ordinary format', function() {
+	var longCmd = 'echo ' + _(40).range().join('');
+
+	describe('with config in normalized format', function() {
 		var projectPath = '/some/path',
-			projectConfig = {someOption: 'someValue'},
+			projectConfig = {
+				steps: [
+					{type: 'shell', cmd: 'echo 1'},
+					{type: 'shell', cmd: longCmd}
+				]
+			},
 			loadedProjectConfig;
+
+		var expectedProjectConfig = {
+			steps: [
+				{
+					name: utils.prune(projectConfig.steps[0].cmd, 40),
+					cmd: 'echo 1',
+					type: 'shell'
+				},
+				{
+					name: utils.prune(projectConfig.steps[1].cmd, 40),
+					cmd: longCmd,
+					type: 'shell'
+				}
+			]
+		};
 
 		before(function() {
 			mocks = getMocks({
-				loadResult: projectConfig
+				loadResult: {
+					steps: _(projectConfig.steps).map(function(step) {
+						return _(step).clone();
+					})
+				}
 			});
 
 			projects = getProjectsCollection(mocks);
@@ -73,9 +100,75 @@ describe('Projcts collection `loadConfig` method', function() {
 
 		checkReaderLoadCall({projectPath: projectPath});
 
-		it('should return false', function() {
-			expect(loadedProjectConfig).eql(projectConfig);
+		it('should return original project config with added names', function() {
+			expect(loadedProjectConfig).eql(expectedProjectConfig);
 		});
+	});
+
+	describe('with config with steps as object', function() {
+		var projectPath = '/some/path',
+			projectConfig = {
+				steps: {
+					'echo 1': 'echo 1',
+					'echo 2': {cmd: longCmd}
+				}
+			},
+			loadedProjectConfig;
+
+		var expectedProjectConfig = {
+			steps: [
+				{name: 'echo 1', cmd: 'echo 1', type: 'shell'},
+				{name: 'echo 2', cmd: longCmd, type: 'shell'}
+			]
+		};
+
+		before(function() {
+			mocks = getMocks({
+				loadResult: {
+					steps: _(projectConfig.steps).clone()
+				}
+			});
+
+			projects = getProjectsCollection(mocks);
+		});
+
+		it('should be called witout errors', function(done) {
+			projects._loadConfig(projectPath, function(err, result) {
+				expect(err).not.ok();
+				loadedProjectConfig = result;
+				done();
+			});
+		});
+
+		checkReaderLoadCall({projectPath: projectPath});
+
+		it('should return normalized project config', function() {
+			expect(loadedProjectConfig).eql(expectedProjectConfig);
+		});
+	});
+
+	describe('when error on reader load', function() {
+		var projectPath = '/some/path',
+			projectConfig = {},
+			loadError = new Error('some error');
+
+		before(function() {
+			mocks = getMocks({
+				loadError: loadError
+			});
+
+			projects = getProjectsCollection(mocks);
+		});
+
+		it('should be called with error', function(done) {
+			projects._loadConfig(projectPath, function(err) {
+				expect(err).equal(loadError);
+				done();
+			});
+		});
+
+		checkReaderLoadCall({projectPath: projectPath});
+
 	});
 
 });
