c26bb7666cf44590241177d3df0c09e8e033ee15
oleg <oleg.korobenko@gmail.com>
2018-11-29 16:16:47 +0300

    pass project envVars to the executor

--- test/distributor/envVars.js
+++ test/distributor/envVars.js
@@ -8,7 +8,12 @@ var expect = require('expect.js'),
 describe('Distributor build env vars usage', function() {
 	var distributor;
 
-	describe('env vars passed to create executor', function() {
+	var nciEnvVars = [
+		'NCI_BUILD_ID', 'NCI_BUILD_NUMBER', 'NCI_PROJECT_NAME', 'NCI_NODE_NAME',
+		'NCI_ENV_NAME'
+	];
+
+	describe('without project env vars', function() {
 		var project = {name: 'project1'},
 			runExecutorSpy,
 			buildId = 20,
@@ -55,6 +60,10 @@ describe('Distributor build env vars usage', function() {
 			envVars = params.envVars;
 		});
 
+		it('should provide only nci specific env vars', function() {
+			expect(envVars).only.have.keys(nciEnvVars);
+		});
+
 		it('should provide NCI_BUILD_ID env var', function() {
 			expect(envVars.NCI_BUILD_ID).equal(String(buildId));
 		});
@@ -74,5 +83,57 @@ describe('Distributor build env vars usage', function() {
 		it('should provide NCI_ENV_NAME env var', function() {
 			expect(envVars.NCI_ENV_NAME).equal(String(env.name));
 		});
+	});
+
+	describe('with project env vars', function() {
+		var project = {
+				name: 'project1',
+				envVars: {
+					someVar: 'someValue'
+				}
+			},
+			runExecutorSpy,
+			env = {name: 'someEnv'},
+			node = {
+				name: 'local',
+				type: 'local',
+				maxExecutorsCount: 1,
+				envs: ['someEnv']
+			};
+
+		it('instance should be created without errors', function() {
+			distributor = helpers.createDistributor({
+				projects: [project],
+				nodes: [node]
+			});
+
+			runExecutorSpy = sinon.spy(distributor.nodes[0], 'runExecutor');
+
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: project.name, env: env}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		var envVars;
+
+		it('should provide env vars for runExecutor call', function() {
+			var params = runExecutorSpy.getCall(0).args[1];
+			expect(params).have.keys('envVars');
+			expect(params.envVars).a('object');
+			envVars = params.envVars;
+		});
+
+		it('should provide nci specific env vars', function() {
+			expect(envVars).have.keys(nciEnvVars);
+		});
+
+		it('should provide project env vars', function() {
+			expect(envVars.someVar).equal('someValue');
+		});
+
 	});
 });
