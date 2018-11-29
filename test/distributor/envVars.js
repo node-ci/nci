43c0e974d9679625e6ef23a299614ac7e6c326f8
oleg <oleg.korobenko@gmail.com>
2018-11-29 16:45:40 +0300

    allow to specify node envVars, pass them to the executor

--- test/distributor/envVars.js
+++ test/distributor/envVars.js
@@ -136,4 +136,166 @@ describe('Distributor build env vars usage', function() {
 		});
 
 	});
+
+	describe('with node env vars', function() {
+		var project = {
+				name: 'project1'
+			},
+			runExecutorSpy,
+			env = {name: 'someEnv'},
+			node = {
+				name: 'local',
+				type: 'local',
+				maxExecutorsCount: 1,
+				envs: ['someEnv'],
+				envVars: {
+					someVar: 'someValue'
+				}
+			};
+
+		it('instance should be created without errors', function() {
+			distributor = helpers.createDistributor({
+				projects: [project],
+				nodes: [node]
+			});
+
+			runExecutorSpy = sinon.spy(distributor.nodes[0], 'runExecutor');
+
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: project.name, env: env}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		var envVars;
+
+		it('should provide env vars for runExecutor call', function() {
+			var params = runExecutorSpy.getCall(0).args[1];
+			expect(params).have.keys('envVars');
+			expect(params.envVars).a('object');
+			envVars = params.envVars;
+		});
+
+		it('should provide nci specific env vars', function() {
+			expect(envVars).have.keys(nciEnvVars);
+		});
+
+		it('should provide node env vars', function() {
+			expect(envVars.someVar).equal('someValue');
+		});
+
+	});
+
+	describe('with overrinding project and node env var', function() {
+		var project = {
+				name: 'project1',
+				envVars: {
+					someVar: 'someProjectValue'
+				}
+			},
+			runExecutorSpy,
+			env = {name: 'someEnv'},
+			node = {
+				name: 'local',
+				type: 'local',
+				maxExecutorsCount: 1,
+				envs: ['someEnv'],
+				envVars: {
+					someVar: 'someNodeValue'
+				}
+			};
+
+		it('instance should be created without errors', function() {
+			distributor = helpers.createDistributor({
+				projects: [project],
+				nodes: [node]
+			});
+
+			runExecutorSpy = sinon.spy(distributor.nodes[0], 'runExecutor');
+
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: project.name, env: env}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		var envVars;
+
+		it('should provide env vars for runExecutor call', function() {
+			var params = runExecutorSpy.getCall(0).args[1];
+			expect(params).have.keys('envVars');
+			expect(params.envVars).a('object');
+			envVars = params.envVars;
+		});
+
+		it('should provide nci specific env vars', function() {
+			expect(envVars).have.keys(nciEnvVars);
+		});
+
+		it('should prefer project env var value', function() {
+			expect(envVars.someVar).equal('someProjectValue');
+		});
+
+	});
+
+	describe('with overrinding nci, project and node env var', function() {
+		var project = {
+				name: 'project1',
+				envVars: {
+					NCI_NODE_NAME: 'someProjectValue'
+				}
+			},
+			runExecutorSpy,
+			env = {name: 'someEnv'},
+			node = {
+				name: 'local',
+				type: 'local',
+				maxExecutorsCount: 1,
+				envs: ['someEnv'],
+				envVars: {
+					NCI_NODE_NAME: 'someNodeValue'
+				}
+			};
+
+		it('instance should be created without errors', function() {
+			distributor = helpers.createDistributor({
+				projects: [project],
+				nodes: [node]
+			});
+
+			runExecutorSpy = sinon.spy(distributor.nodes[0], 'runExecutor');
+
+		});
+
+		it('should run without errors', function(done) {
+			distributor.run({projectName: project.name, env: env}, function(err) {
+				expect(err).not.ok();
+				done();
+			});
+		});
+
+		var envVars;
+
+		it('should provide env vars for runExecutor call', function() {
+			var params = runExecutorSpy.getCall(0).args[1];
+			expect(params).have.keys('envVars');
+			expect(params.envVars).a('object');
+			envVars = params.envVars;
+		});
+
+		it('should provide nci specific env vars', function() {
+			expect(envVars).have.keys(nciEnvVars);
+		});
+
+		it('should prefer nci specific env var value', function() {
+			expect(envVars.NCI_NODE_NAME).equal(node.name);
+		});
+
+	});
 });
